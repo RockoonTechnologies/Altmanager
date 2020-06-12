b9dd27a10af83b2455bf7cead33b57b378586c58
RockoonTechnologies <66290320+RockoonTechnologies@users.noreply.github.com>
2020-06-11 22:24:03 -0400

    fixed craps

--- demo/demo.ino
+++ demo/demo.ino
@@ -1,25 +1,25 @@
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_BMP280.h>
+#include <Wire.h> // imports dependency
+#include <SPI.h>  // imports dependency
+#include <Adafruit_BMP280.h> // imports dependency
 
-#include <altmanager.h>
+#include <altmanager.h> // imports lib
 
 
 
 float alt;
-float alt;
+float alt2;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // how many times you want to average starts serial
   Serial.println(F("BMP280 test"));
-  start(0x76);
-  average(5);
+  start(0x76); //set this to your desired i2c address
+  average(5); // how many times you want to average
  
 }
 
 void loop() {
     
     delay(2000);
-    alt = getAlt();
-    alt2 = rawAlt();
+    alt = getAlt(); // gets alt based on average
+    alt2 = rawAlt(); // gets alt based on sealevel
 }
